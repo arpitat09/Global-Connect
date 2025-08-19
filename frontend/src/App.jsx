5f8012d503a95b69cc454731c61ce04e5854ef65
arpitat09 <arpita.genai@gmail.com>
2025-08-19 23:23:03 +0530

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import MainLayout from "./layouts/MainLayout";
+import Job from "./pages/Job";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile/:id" element={<Profile />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/jobs" element={<Job />} />
+      </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
+}
